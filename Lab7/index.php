4101f8f88079bf28a23c8fcb0742c60122746973
HarithThaqif <102360522+HarithThaqif@users.noreply.github.com>
2022-06-06 22:30:24 +0800

    Create index.php

---
+++ Lab7/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>AUTOS DATABASE 205823 Harith Thaqif</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=devicewidth, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+  <link type="text/css" rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="jumbotron" id="head">
+       <h1> Welcome to Autos Database</h1>
+     </div>
+     <div class="container">
+           <p><a href="login.php">Please log in</a></p>
+           <p>Attempt to go to <a href="javascript:void(0)" data-toggle="modal" data-target="#myModal">autos.php</a> without loggin in - it should fail with an error message<p>
+ <p><a href="#">Specification for this Application</a></p>
+
+
+
+ <div class="modal fade" id="myModal" role="dialog">
+<div class="modal-dialog">
+
+<!-- Modal content-->
+<div class="modal-content">
+<div class="modal-header">
+<button type="button" class="close" data-dismiss="modal">&times;</button>
+<h4 class="modal-title">Error Message</h4>
+</div>
+<div class="modal-body">
+<p>You must login to access autos.php</p>
+</div>
+<div class="modal-footer">
+<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+</div>
+</div>
+
+</div>
+</div>
+     </div>
+</body>
+</html>
